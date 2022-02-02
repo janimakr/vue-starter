e2a94795c4031715509e287dd4cdffc2634dc262
cb-mathu <mathumitha@chargebee.com>
2022-02-02 18:28:43 +0530

    Test case setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: "ts-jest",
+  moduleFileExtensions: ["ts", "js", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+  },
 };
